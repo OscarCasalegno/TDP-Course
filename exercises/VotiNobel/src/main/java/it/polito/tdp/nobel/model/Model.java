0efd3fa4e1ff4f248af5f493c7974da80940e07b
OscarCasalegno <61799933+OscarCasalegno@users.noreply.github.com>
2021-11-09 00:41:27 +0100

    Add files via upload

---
+++ exercises/VotiNobel/src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -0,0 +1,15 @@
+package it.polito.tdp.nobel.model;
+
+import java.util.Set;
+
+public class Model {
+
+	
+	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
+		
+		System.out.println("TODO!");
+		
+		return null;
+	}
+
+}
